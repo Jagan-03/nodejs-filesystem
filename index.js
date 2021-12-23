75d0f2c87508f4f3f984b2d37492a692a75ef25f
Jaganath Ezhilarasu <jaganmj324@gmail.com>
2021-12-23 11:26:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+app.use('/public', express.static(__dirname + '/public'));  
+app.use(express.static(__dirname + '/public')); 
+
+app.get("/", (req, res) => {
+    const directoryName = __dirname + "/public/time";
+    const fileNames = fs.readdirSync(directoryName);
+    res.send(fileNames);
+})
+
+app.post("/", (req, res, err) => {
+    try {
+        const today = new Date();
+        const date = `${today.getDate()}-${today.getMonth()+1}-${today.getFullYear()}`;
+        const time = `${today.getHours()}-${today.getMinutes() < 10 ? "0"+today.getMinutes() : today.getMinutes()}`;
+        const filename = `${date}_${time}`;
+        const filepath = __dirname + `/public/time/${filename}.txt`;
+        
+        fs.appendFile(filepath, today.toString(), (err) => {
+            if (err) throw err;
+            res.send("Timestamp saved");
+            console.log("saved");
+        })
+
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+app.listen(3001, () => {
+    console.log("listening on port 3001");
+})
